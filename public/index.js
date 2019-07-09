6ab6202fb66aaec4c3455678d8c524e1742b9ece
Allison Geismar <ageismar213@gmail.com>
2019-07-08 20:31:10 -0400

    able to click certain muscles and have a gui render

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-
 var SCREEN_WIDTH = window.innerWidth;
 var SCREEN_HEIGHT = window.innerHeight;
 
@@ -55,10 +54,6 @@ function init() {
 
   character = new THREE.UCSCharacter();
 
-
-  gui = new dat.GUI();
-  gui.width = 300;
-  gui.open();
   //};
 
   var loader = new THREE.XHRLoader();
@@ -106,13 +101,67 @@ function init() {
 //     skinGui.open();
 //   }
 
+function displayEx(arr, gooey) {
+  for (var i = 0; i < arr.length; i++) {
+    exerciseConfig[arr[i]] = function() {
+      return null;
+    };
+    gooey.add(exerciseConfig, arr[i]);
+  }
+}
+
+var pectorialEx = [
+  'Bench Press',
+  'Renegade Rows',
+  'Push Ups',
+  'Chest Press',
+  'Cable Flyes',
+  'Dips'
+];
+var abdominalEx = [
+  'Crunches',
+  'Plank',
+  'Sit Ups',
+  'Leg Raises',
+  'Navy Kicks',
+  'Russian Twists'
+];
+var calfEx = ['Calf Raises', 'Skater Jumps'];
+var quadsEx = ['Sqauts', 'Step Ups', 'Lunges', 'Leg Extensions', 'Box Jumps'];
+var bicepEx = [
+  'Bicep Curls',
+  'Hammer Curls',
+  'Preacher Curls',
+  'Zottoman Curls'
+];
 function setupGUI(intersectedObj) {
+  if (gui) {
+    gui.destroy();
+    exerciseConfig = {
+      wireframe: false
+    };
+  }
+  gui = new dat.GUI();
+  gui.width = 250;
+  gui.open();
   var morphGui = gui.addFolder(intersectedObj.name + ' Exercises');
 
+  //this doesn't work!
 
-  for (var i = 0; i < character.numMorphs; i++) {
-    morphGui
-      .add(null, 'Crunches');
+  exerciseConfig = {
+    wireframe: false
+  };
+
+  if (intersectedObj.name === 'Pectorial') {
+    displayEx(pectorialEx, morphGui);
+  } else if (intersectedObj.name === 'Abdominal') {
+    displayEx(abdominalEx, morphGui);
+  } else if (intersectedObj.name === 'Gastrocnemius') {
+    displayEx(calfEx, morphGui);
+  } else if (intersectedObj.name === 'Quadriceps') {
+    displayEx(quadsEx, morphGui);
+  } else if (intersectedObj.name === 'Biceps') {
+    displayEx(bicepEx, morphGui);
   }
 
   morphGui.open();
@@ -143,32 +192,133 @@ function onDocumentMouseDown(e) {
     -(e.clientY / window.innerHeight) * 2 + 1
   );
 
-
-  raycaster.setFromCamera(mouseVector.clone(),camera)
+  raycaster.setFromCamera(mouseVector.clone(), camera);
 
   // create an array containing all objects in the scene with which the ray intersects
-  var intersects = raycaster.intersectObjects( scene.children );
-    console.log('intersects', intersects)
-  if (intersects.length>0){
-    console.log("Intersected object:", intersects.length);
-    console.log(gui)
-    if(!gui.__folders['Abs Exercises']) setupGUI(intersects[0].object)
-    else console.log('this already exists')
-}
+  var intersects = raycaster.intersectObjects(scene.children);
+  if (intersects.length > 0) {
+    setupGUI(intersects[0].object);
+  }
 }
 
-//circle
-var geometry = new THREE.CircleGeometry(100, 32);
+//circle - abs
+var geometry = new THREE.PlaneBufferGeometry(350, 900, 2);
 var material = new THREE.MeshBasicMaterial({
   color: 0x000000,
-  opacity: 0.5,
+  opacity: 0,
   transparent: true
 });
 var circle = new THREE.Mesh(geometry, material);
-circle.position.set(0, 3500, 70);
+circle.position.set(0, 3600, 70);
 circle.rotation.x -= 0.22;
-circle.name = 'Abs'
+circle.name = 'Abdominal';
 scene.add(circle);
+
+//circle - pecs
+const planeGeometry = new THREE.PlaneBufferGeometry(500, 300, 2);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xf00000,
+  opacity: 0,
+  transparent: true
+});
+var square = new THREE.Mesh(planeGeometry, material);
+square.position.set(0, 4350, -20);
+square.rotation.x -= 0.4;
+square.name = 'Pectorial';
+scene.add(square);
+
+//bone - calf
+var boneGeo = new THREE.CylinderBufferGeometry(
+  200,
+  100,
+  1000,
+  500,
+  1500,
+  500,
+  3000
+);
+
+var material = new THREE.MeshBasicMaterial({
+  color: 0x0f0000,
+  opacity: 0,
+  transparent: true
+});
+var bone = new THREE.Mesh(boneGeo, material);
+bone.position.set(300, 1000, -500);
+bone.rotation.x += 0.2;
+bone.name = 'Gastrocnemius';
+scene.add(bone);
+
+var boneGeo2 = new THREE.CylinderBufferGeometry(
+  200,
+  100,
+  1000,
+  500,
+  1500,
+  500,
+  3000
+);
+
+var material = new THREE.MeshBasicMaterial({
+  color: 0x0f0000,
+  opacity: 0,
+  transparent: true
+});
+var bone2 = new THREE.Mesh(boneGeo2, material);
+bone2.position.set(-300, 1000, -500);
+bone2.rotation.x += 0.2;
+bone2.name = 'Gastrocnemius';
+scene.add(bone2);
+
+//planes - quads
+const quadsGeometry = new THREE.PlaneBufferGeometry(270, 700, 2);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xf00000,
+  opacity: 0,
+  transparent: true
+});
+var quads = new THREE.Mesh(quadsGeometry, material);
+quads.position.set(-250, 2050, -100);
+quads.rotation.x += 0.2;
+quads.name = 'Quadriceps';
+scene.add(quads);
+
+const quadsGeometry2 = new THREE.PlaneBufferGeometry(270, 700, 2);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xf00000,
+  opacity: 0,
+  transparent: true
+});
+var quads2 = new THREE.Mesh(quadsGeometry2, material);
+quads2.position.set(250, 2050, -100);
+quads2.rotation.x += 0.2;
+quads2.name = 'Quadriceps';
+scene.add(quads2);
+
+//Biceps
+const bicepGeometry = new THREE.PlaneBufferGeometry(400, 200, 2);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xf00000,
+  opacity: 0,
+  transparent: true
+});
+var bicep = new THREE.Mesh(bicepGeometry, material);
+bicep.position.set(-950, 4300, -350);
+bicep.rotation.x += 0.2;
+bicep.name = 'Biceps';
+scene.add(bicep);
+
+const bicepGeometry2 = new THREE.PlaneBufferGeometry(400, 200, 2);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xf00000,
+  opacity: 0,
+  transparent: true
+});
+var bicep2 = new THREE.Mesh(bicepGeometry2, material);
+bicep2.position.set(950, 4300, -350);
+bicep2.rotation.x += 0.2;
+bicep2.name = 'Biceps';
+scene.add(bicep2);
 
 function animate() {
   requestAnimationFrame(animate);
